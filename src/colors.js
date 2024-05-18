e03ac2c281c29a6f51c0d8dbb5d43521cef42583
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-05-18 17:04:47 +0700

    chore: create default colors file

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+export const defaultColors = {
+  mainColor: {
+    monokaiHaze: '#f8f8f2',
+    monokaiGreen: '#a6e22e',
+    monokaiOrange: '#fd971f',
+    monokaiBlue: '#66d9ef',
+    emerald: '#34d399',
+  },
+
+  eerieBlack: '#1d2021',
+}
